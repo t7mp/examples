66f63fff3145cdbda3fe1e9ab8f16630f25b2a31
Joerg Bellmann <joerg.bellmann@googlemail.com>
2012-10-24 18:22:07 +0200

    example for providing external resources added

---
+++ provide-from-external/Readme.md
@@ -0,0 +1,5 @@
+Simple example for using 'libs'-configuration with 'maven-t7-plugin'.
+
+Run 'mvn clean install t7:run' and point your Browser to http://localhost:8080/external-libs/.
+
+It should show you the version of DerbyDB used in this project.
---
+++ provide-from-external/src/test/java/com/googlecode/t7mp/extlib/ResolveVersionIT.java
@@ -0,0 +1,63 @@
+package com.googlecode.t7mp.extlib;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple Test to invoke the webapplication in an
+ * Integration-Test.
+ * 
+ * @author Joerg Bellmann
+ *
+ */
+public class ResolveVersionIT {
+	
+	private static final Logger logger = LoggerFactory.getLogger(ResolveVersionIT.class);
+	
+	@Test
+	public void testResolveVersion() throws ClientProtocolException, IOException{
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet getRequest = new HttpGet("http://localhost:9091/external-libs");
+		client.execute(getRequest, new ResponseHandler<Void>(){
+			@Override
+			public Void handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+				int statusCode = response.getStatusLine().getStatusCode();
+				logger.info("Server returned statusCode: {}", statusCode);
+				Assert.assertEquals(200, statusCode);
+				if(statusCode == HttpStatus.SC_OK){
+					String result = EntityUtils.toString(response.getEntity(), Charset.defaultCharset());
+					logger.info("Server returned: '{}'", result);
+				}
+				return null;
+			}
+		});
+	}
+	
+	@Test
+	public void testResolveClient() throws ClientProtocolException, IOException{
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpGet getRequest = new HttpGet("http://localhost:9091/client");
+		client.execute(getRequest, new ResponseHandler<Void>(){
+			@Override
+			public Void handleResponse(HttpResponse response) throws ClientProtocolException, IOException {
+				int statusCode = response.getStatusLine().getStatusCode();
+				logger.info("Server returned statusCode: {}", statusCode);
+				Assert.assertEquals(200, statusCode);
+				return null;
+			}
+		});
+	}
+
+}
